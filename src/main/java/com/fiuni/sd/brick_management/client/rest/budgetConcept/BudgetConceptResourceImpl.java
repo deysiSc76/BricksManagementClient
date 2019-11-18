59542eab935d977f4952fc48e4da9a65578b27fa
deysiSc76 <31452987+deysiSc76@users.noreply.github.com>
2019-11-18 07:38:42 -0400

    update budgetConcept

--- src/main/java/com/fiuni/sd/brick_management/client/rest/budgetConcept/BudgetConceptResourceImpl.java
+++ src/main/java/com/fiuni/sd/brick_management/client/rest/budgetConcept/BudgetConceptResourceImpl.java
@@ -1,11 +1,9 @@
 package com.fiuni.sd.brick_management.client.rest.budgetConcept;
 
 import com.fiuni.sd.brick_management.client.rest.base.BaseResourceImpl;
-import com.fiuni.sd.brick_management.client.rest.budgetDetail.IBudgetDetailResource;
 import com.fiuni.sd.bricks_management.dto.budgetConcept.BudgetConceptDTO;
 import com.fiuni.sd.bricks_management.dto.budgetConcept.BudgetConceptResult;
-import com.fiuni.sd.bricks_management.dto.budgetDetail.BudgetDetailDTO;
-import com.fiuni.sd.bricks_management.dto.budgetDetail.BudgetDetailResult;
+
 
 public class BudgetConceptResourceImpl extends BaseResourceImpl<BudgetConceptDTO> implements IBudgetConceptResource {
     public BudgetConceptResourceImpl() {
